1f75714a3451ee16b511cd6dc7039bbec58658e5
Provashish Roy <provashishroy95@gmail.com>
2022-04-13 16:34:29 +0600

    load data and show data on ui

---
+++ src/Pages/Home/Service/Service.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Service = ({ service }) => {
+    const { name, price, img, description } = service;
+
+    return (
+        <div>
+            <img src={img} alt="" />
+            <h2>{name}</h2>
+            <p>Price: {price}</p>
+            <p>{description}</p>
+        </div>
+    );
+};
+
+export default Service;
---
+++ src/Pages/Home/Services/Services.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from 'react';
+import Service from '../Service/Service';
+import './Services.css';
+
+const Services = () => {
+    const [services, setServices] = useState([]);
+
+    useEffect(() => {
+        fetch('services.json')
+            .then(res => res.json())
+            .then(data => setServices(data))
+    }, []);
+
+    console.log(services);
+    return (
+        <div>
+            <h2>Services: {services.length}</h2>
+            {
+                services.map(service => <Service key={service.id} service={service}></Service>)
+            }
+        </div>
+    );
+};
+
+export default Services;
