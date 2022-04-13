cbcc20fcb0bbfc93ddd4f30ee94ae94143cee48e
Provashish Roy <provashishroy95@gmail.com>
2022-04-13 17:07:13 +0600

    added bootstrap

--- src/Pages/Home/Experts/Experts.js
+++ src/Pages/Home/Experts/Experts.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Experts.css';
+import experts1 from '../../../images/experts/expert-1.jpg';
 
 const Experts = () => {
     return (
         <div>
-            <h2>Our Experts </h2>
+            <h2 className='text-info'>Our Experts </h2>
+            <img src={experts1} alt="" />
         </div>
     );
 };
