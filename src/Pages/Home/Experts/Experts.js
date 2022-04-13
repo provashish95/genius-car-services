bf33b2c58a8a65151587541151e40658a0382840
Provashish Roy <provashishroy95@gmail.com>
2022-04-13 17:10:24 +0600

    updated

--- src/Pages/Home/Experts/Experts.js
+++ src/Pages/Home/Experts/Experts.js
@@ -4,8 +4,8 @@ import experts1 from '../../../images/experts/expert-1.jpg';
 
 const Experts = () => {
     return (
-        <div>
-            <h2 className='text-info'>Our Experts </h2>
+        <div className='container'>
+            <h2 className='text-info text-center'>Our Experts </h2>
             <img src={experts1} alt="" />
         </div>
     );
