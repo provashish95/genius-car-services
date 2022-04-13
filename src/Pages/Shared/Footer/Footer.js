181ac0b315425fa3fc97f63ba80a9e7a29e3a8a2
Provashish Roy <provashishroy95@gmail.com>
2022-04-13 16:15:03 +0600

    add components

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
     let date = new Date().getFullYear();
-    //console.log(date);
+
     return (
         <footer>
             <small>&copy; Copyright {date}</small>
