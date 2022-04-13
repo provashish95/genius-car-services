1124507668983eadfc348571c99faf06a5edbd3e
Provashish Roy <provashishroy95@gmail.com>
2022-04-13 16:59:22 +0600

    added two experts components

---
+++ src/Pages/Home/Expert/Expert.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Expert.css';
+
+const Expert = () => {
+    return (
+        <div>
+            <h2>Our Expert</h2>
+        </div>
+    );
+};
+
+export default Expert;
---
+++ src/Pages/Home/Experts/Experts.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Experts.css';
+
+const Experts = () => {
+    return (
+        <div>
+            <h2>Our Experts </h2>
+        </div>
+    );
+};
+
+export default Experts;
