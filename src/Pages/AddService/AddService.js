fe55872080e5446ed2a1d724d8a4720fe56a93de
Provashish Roy <provashishroy95@gmail.com>
2022-04-24 12:18:47 +0600

    added data to server

--- src/Pages/AddService/AddService.js
+++ src/Pages/AddService/AddService.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import { useForm } from "react-hook-form";
 
 const AddService = () => {
+    const { register, handleSubmit } = useForm();
+
+    const onSubmit = data => {
+        console.log(data);
+        const url = `http://localhost:5000/service`;
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+            .then(res => res.json())
+            .then(result => {
+                console.log(result)
+            })
+    }
+
     return (
-        <div>
-            <h1>This is add service</h1>
+        <div className='w-50 mx-auto'>
+            <h3 className='text-center'>This is add service</h3>
+            <form onSubmit={handleSubmit(onSubmit)} className="d-flex flex-column" >
+                <input className='mb-2' placeholder='Enter your name' {...register("name", { required: true })} />
+                <textarea className='mb-2' placeholder='Write your description' {...register("description")} />
+                <input className='mb-2' placeholder='Enter your price' type="number" {...register("price")} />
+                <input className='mb-2' placeholder='Enter your Photo' type="text" {...register("img")} />
+                <input type="submit" value="Add Service" />
+            </form>
         </div>
     );
 };
