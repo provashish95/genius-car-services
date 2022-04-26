f47aed57b22253268903eed43287bd721ef8bdce
Provashish Roy <provashishroy95@gmail.com>
2022-04-27 00:20:58 +0600

    insert data to database

--- src/Pages/Checkout/Checkout/Checkout.js
+++ src/Pages/Checkout/Checkout/Checkout.js
@@ -3,6 +3,8 @@ import { useParams } from 'react-router-dom';
 import useServiceDetails from '../../../useServices/useServiceDetails';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import auth from '../../../firebase.init';
+import axios from 'axios';
+import { toast } from 'react-toastify';
 
 const Checkout = () => {
     const [user] = useAuthState(auth);
@@ -35,6 +37,14 @@ const Checkout = () => {
             address: event.target.address.value,
             phone: event.target.phone.value
         }
+        axios.post('http://localhost:5000/order', order)
+            .then(response => {
+                const { data } = response;
+                if (data.insertedId) {
+                    toast.success("Your order is booked");
+                    event.target.reset();
+                }
+            })
     }
 
     return (
@@ -43,8 +53,8 @@ const Checkout = () => {
             <form onSubmit={handlePlaceOrder}>
                 <input className='w-100 mb-2' type='text' value={user.displayName} name='name' placeholder='Enter your name' required readOnly disabled /><br />
                 <input className='w-100 mb-2' type='email' value={user.email} name='email' placeholder='Enter your email' required readOnly disabled /><br />
+                <input className='w-100 mb-2' type='text' value={service.name} name='service' placeholder='Enter your service' required readOnly /><br />
                 <input className='w-100 mb-2' type='text' name='address' placeholder='Enter your address' required autoComplete='off' /><br />
-                <input className='w-100 mb-2' type='text' value={service.name} name='service' placeholder='Enter your service' required /><br />
                 <input className='w-100 mb-2' type='text' value={user.phone} name='phone' placeholder='Enter your phone' required /><br />
                 <input className='btn btn-primary' type="submit" value="Please Order" />
             </form>
--- src/Pages/Login/SocialLogin/SocialLogin.js
+++ src/Pages/Login/SocialLogin/SocialLogin.js
@@ -4,7 +4,7 @@ import github from '../../../images/social/github.png';
 import facebook from '../../../images/social/facebook.png';
 import { useSignInWithFacebook, useSignInWithGithub, useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import auth from '../../../firebase.init';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import Loading from '../../Shared/Loading/Loading';
 
 const SocialLogin = () => {
@@ -13,6 +13,9 @@ const SocialLogin = () => {
     const [signInWithGithub, gitUser, gitLoading, gitError] = useSignInWithGithub(auth);
     const [signInWithFacebook, facebookUser, facebookLoading, facebookError] = useSignInWithFacebook(auth);
     let errorElement;
+    const location = useLocation();
+
+    let from = location.state?.from?.pathname || "/";
 
     if (loading || gitLoading || facebookLoading) {
         return <Loading></Loading>
@@ -23,7 +26,7 @@ const SocialLogin = () => {
     }
 
     if (user || gitUser || facebookUser) {
-        navigate('/home');
+        navigate(from, { replace: true });
     }
 
 
