4c0acab2be2536ea65fb76ab423a49bfeaed13a1
Provashish Roy <provashishroy95@gmail.com>
2022-04-27 13:11:16 +0600

    get orders by user email

--- src/Pages/Order/Order.js
+++ src/Pages/Order/Order.js
@@ -1,9 +1,26 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 
 const Order = () => {
+    const [user] = useAuthState(auth);
+    const [orders, setOrders] = useState([])
+    useEffect(() => {
+        const getOrders = async () => {
+            const email = user.email;
+            const url = `http://localhost:5000/order?email=${email}`;
+            const { data } = await axios.get(url);
+            setOrders(data);
+        }
+        getOrders();
+    }, [user]);
+
+
     return (
         <div>
-            <h4>This is order page</h4>
+            <h4>Your orders: {orders.length}</h4>
         </div>
     );
 };
