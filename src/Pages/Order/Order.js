83d77dbaa051606fc4813f183b6858b0929c7747
Provashish Roy <provashishroy95@gmail.com>
2022-04-27 14:06:51 +0600

    acces token and verify token

--- src/Pages/Order/Order.js
+++ src/Pages/Order/Order.js
@@ -8,10 +8,15 @@ const Order = () => {
     const [user] = useAuthState(auth);
     const [orders, setOrders] = useState([])
     useEffect(() => {
+
         const getOrders = async () => {
             const email = user.email;
             const url = `http://localhost:5000/order?email=${email}`;
-            const { data } = await axios.get(url);
+            const { data } = await axios.get(url, {
+                headers: {
+                    authorization: `Bearer ${localStorage.getItem('accessToken')}`
+                }
+            });
             setOrders(data);
         }
         getOrders();
